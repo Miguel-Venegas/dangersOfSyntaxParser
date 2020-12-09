b7bac6de0a0c15fdd22e2e5533544dd5d963818b
Miguel_Venegas <MiguelVenegas@Miguels-MBP.routerlogin.net>
2020-12-09 05:38:52 -0800

    dangers of syntax parser

---
+++ Starter/app.js
@@ -0,0 +1,20 @@
+// in this example, I learned the dangers of syntax parser. The syntax parsers automatically inserts a semicolon when it sees return statement and nothing else on the same line. That can be a problem, like in the example below, when accidentally starting a new line, yet still part of the return statement's purview, as it may return undefined.
+
+function getPerson(){
+    return
+    {
+        firstName:'Miguel'
+    }
+}
+
+console.log(getPerson()); // expected output: object {firstName: 'Miguel'} vs actual output: 
+                        
+// to fix said problem, simply add the bracket in the same line as the return so that the syntax parser knows that you want to return whatever is on the same line as the return statement
+
+function getperson(){
+    return {
+        firstName:'Miguel'
+    }
+}
+
+console.log(getperson()); // output: {firstName: 'Miguel'}
